5235eb4c602c7865099d288cd2e478700ec3dacd
tuanduongnlu <letuanduong682000@gmail.com>
2020-06-25 01:01:27 +0700

    thêm thư viện

--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 class Result extends Component {
     render() {
         return (
             <div>
                  <div id="align-top"></div>
-                 
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
-                <h1>Resultttttttttt</h1>
+                 <Tabs>
+    <TabList>
+      <Tab>Đánh Giá Quá Trình</Tab>
+      <Tab>Bài Học Tích Lũy</Tab>
+    </TabList>
+ 
+    <TabPanel>
+      <h2>Any content 1</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Any content 2</h2>
+    </TabPanel>
+  </Tabs>
+               
             </div>
         );
     }
