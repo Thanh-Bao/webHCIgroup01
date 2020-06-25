e23c6be44aaa5f2fd6eca515c3aa126a9ed3d673
Minh-Huan <66371006+ElitotHuan@users.noreply.github.com>
2020-06-25 16:39:36 +0700

    Footer
    Thêm Footer vào trang web

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,131 @@
 
 import React, { Component } from "react";
-
+import '../CSS/Footer/Footer.css'
+import { NavLink } from "react-router-dom";
 class Footer extends Component {
   render() {
     return (
-        <div>
-          <h1>Footer</h1>
-          <h1>Footer</h1>
-          <h1>Footer</h1>
-          <h1>Footer</h1>
-          <h1>Footer</h1>
-          <h1>Footer</h1>
-          <h1>Footer</h1>
-          <h1>Footer</h1>
-          <h1>Footer</h1>
-          <h1>Footer</h1>
-          <h1>Footer</h1>
-          <h1>Footer</h1>
-          <h1>Footer</h1>    
-        </div>
+      <div>
+        <footer className="webintern-footer">
+          <div className="webintern-footer-inner">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-6 col-md-3 ">
+                  <a className="webintern-footer-logo" href="#">
+                    <img className="img-responsive" src="https://www.freelogodesign.org/img/logo-ex-4.png" style={{ width: '100px' }} alt="" />
+                  </a>
+                  <div className="simple-text dark padding-sm">
+                    <p>Totam rem aperiam, eaque ipsa quae ab illo inv ent ore veritatis et quasi architecto beatae vitae dict eaque ipsa quae ab.</p>
+                  </div>
+                  <div className="empty-space xs-25 sm-25" />
+                </div>
+                <div className="col-sm-6 col-md-2 footer-2">
+                  {/* webintern_footer_title */}
+                  <h4 className="webintern_footer_title h5"><small>Quick Links</small></h4>
+                  <ul className="webintern-footer-list">
+                    <NavLink
+                      className="nav-link"
+                      to="/home"
+                      activeClassName="current_content"
+                    >
+                      <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4" style={{ color: 'white' }}>
+                        <span>
+                          <i className="fa fa-home"></i> Home
+                          </span>
+                      </li>
+                    </NavLink>
+
+                    <NavLink
+                      className="nav-link"
+                      to="/diary"
+                      activeClassName="current_content"
+                    >
+                      <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4"  style={{ color: 'white' }}>
+                        <span>
+                          <i className="fas fa-history"></i> Diary
+                          </span>
+                      </li>
+                    </NavLink>
+                    <NavLink
+                      className="nav-link"
+                      to="/about"
+                      activeClassName="current_content"
+                    >
+                      <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4"  style={{ color: 'white' }}>
+                        <span>
+                          <i className="far fa-address-card"></i> About
+                          </span>
+                      </li>
+                    </NavLink>
+                    <NavLink
+                      className="nav-link"
+                      to="/result"
+                      activeClassName="current_content"
+                    >
+                      <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4"  style={{ color: 'white' }}>
+                        <span>
+                          <i class="fas fa-trophy"></i> Result
+                          </span>
+                      </li>
+                    </NavLink>
+                  </ul>
+                  <div className="empty-space xs-25 sm-25" />
+                </div>
+                <div className="col-sm-6 col-md-3 footer-3">
+                  {/* webintern_footer_title */}
+                  <h4 className="webintern_footer_title h5"><small> Our Courses </small></h4>
+                  <ul className="webintern-footer-list">
+                    <li><a href="#">Human-Computer Interaction </a></li>
+                    <li><a href="#">Object-Oriented Design</a></li>
+                    <li><a href="#">Intro Database</a></li>
+                    <li><a href="#">Graph Theory</a></li>
+                  </ul>
+                  <div className="empty-space xs-25" />
+                </div>
+                <div className="col-xs-12 col-md-4 col-sm-6">
+                  <div className="marg-sm-b30" />
+                  {/* webintern_footer_title */}
+                  <h4 className="webintern_footer_title h5"><small>Subscribe Newsletter</small></h4>
+                  <div className="empty-space marg-lg-b20" />
+                  <div className="empty-space marg-lg-b15" />
+                  {/* TT-SUBSCRIBE */}
+                  <form method="post">
+                    <div className="tt-subscribe">
+                      <input type="text" required placeholder="Enter your email" />
+                      <div className="tt-subscribe-submit">
+                        <i className="fa fa-envelope" aria-hidden="true" />
+                        <input type="submit" defaultValue />
+                      </div>
+                    </div>
+                  </form>
+                  <div className="empty-space marg-lg-b30" />
+                  {/* TT-SOCAIL */}
+                  <ul className="tt-socail">
+                    <li><a href="https://www.facebook.com/groups/605810996700128"><i className="fa fa-facebook" aria-hidden="true" /></a></li>
+                    <li><a href="#"><i className="fa fa-linkedin" aria-hidden="true" /></a></li>
+                    <li><a href="#"><i className="fa fa-twitter" aria-hidden="true" /></a></li>
+                    <li><a href="#"><i className="fa fa-google-plus" aria-hidden="true" /></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="tt-copy">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-6">
+                  <div className="tt-copy-left">Copyright © company 2018. All rights reserved. </div>
+                </div>
+                <div className="col-sm-6">
+                  <div className="tt-copy-right">
+                    Created by: Huan
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
     );
   }
 }
