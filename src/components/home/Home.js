90229fb04cc08f92c6a5c8d22dbcf5cd21bab4c9
apeo8a123 <64093081+apeo8a123@users.noreply.github.com>
2020-06-26 23:52:08 +0700

    add Home.js
    thêm home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
+import "./Home/styte.css"
 
 class Home extends Component {
     render() {
+
         return (
             <div>
-                <div id="align-top"></div>
-
-                <h1>home1</h1>
-                <h1>home2</h1>
-                <h1>home3</h1>
-                <h1>home</h1>
-                <h1>home</h1>
-                <h1>home</h1>
-                <h1>home</h1>
-                <h1>home</h1>
-                <h1>home</h1>
-                <h1>home</h1>
-                <h1>home</h1>
-                <h1>home</h1>
-                <h1>home</h1>
-                <h1>home</h1>
-                <h1>home</h1>
-                <h1>home</h1>
+                <div className="cont">
+                    <div className="mouse" />
+                    <div className="app">
+                        <div className="app__bgimg">
+                            <div className="app__bgimg-image app__bgimg-image--1">
+                            </div>
+                            <div className="app__bgimg-image app__bgimg-image--2">
+                            </div>
+                        </div>
+                        <div className="app__img">
+                            <img onmousedown="return false" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/537051/whiteTest4.png" alt="city" />
+                        </div>
+                        <div className="app__text app__text--1">
+                            <div className="app__text-line app__text-line--4">Welcome </div>
+                            <div className="app__text-line app__text-line--3">HCI_Group_01</div>
+                            <div className="app__text-line app__text-line--2"></div>
+                            <div className="app__text-line app__text-line--1">Năm 2020</div>
+                        </div>
+                        <div className="app__text app__text--2">
+                            <div className="app__text-line app__text-line--4"></div>
+                            <div className="app__text-line app__text-line--3"></div>
+                            <div className="app__text-line app__text-line--2"></div>
+                            <div className="app__text-line app__text-line--1"><img src="" alt="" /></div>
+                        </div>
+                    </div>
+                    <div className="pages">
+                        <ul className="pages__list">
+                            <li data-target={1} className="pages__item pages__item--1 page__item-active" />
+                            <li data-target={2} className="pages__item pages__item--2" />
+                        </ul>
+                    </div>
+                </div>
             </div>
+
         );
     }
 }
