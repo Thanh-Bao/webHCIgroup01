a6b62c4d2a2ba0e76e54b94954e01cf5dfe16793
apeo8a123 <64093081+apeo8a123@users.noreply.github.com>
2020-06-27 13:50:00 +0700

    add search

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,88 +6,91 @@ class NavigationBar extends Component {
   render() {
     return (
       <div>
-      <div className="navigation-wrap bg-light start-header start-style">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <nav className="navbar navbar-expand-md navbar-light">
-                <a className="navbar-brand" href="/">
-                  <img src="media/logo.png" alt="Group 01" />
-                </a>
+        <div className="navigation-wrap bg-light start-header start-style">
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <nav className="navbar navbar-expand-md navbar-light">
+                  <a className="navbar-brand" href="/">
+                    <img src="media/logo.png" alt="Group 01" />
+                  </a>
 
-                {/* menu button for mobile */}
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon" />
-                </button>
-                {/* ___________________________________________ */}
-                <div
-                  className="collapse navbar-collapse"
-                  id="navbarSupportedContent"
-                >
-                  <ul className="navbar-nav ml-auto py-4 py-md-0">
-                    <NavLink
-                      className="nav-link"
-                      to="/home"
-                      activeClassName="current_content"
-                    >
-                      <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                        <span>
-                          <i className="fa fa-home"></i> Home
+                  {/* menu button for mobile */}
+                  <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span className="navbar-toggler-icon" />
+                  </button>
+                  {/* ___________________________________________ */}
+                  <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                  >
+                    <ul className="navbar-nav ml-auto py-4 py-md-0">
+                      <NavLink
+                        className="nav-link"
+                        to="/home"
+                        activeClassName="current_content"
+                      >
+                        <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                          <span>
+                            <i className="fa fa-home"></i> Home
                         </span>
-                      </li>
-                    </NavLink>
+                        </li>
+                      </NavLink>
 
-                    <NavLink
-                      className="nav-link"
-                      to="/diary"
-                      activeClassName="current_content"
-                    >
-                      <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                        <span>
-                          <i className="fas fa-history"></i> Diary
+                      <NavLink
+                        className="nav-link"
+                        to="/diary"
+                        activeClassName="current_content"
+                      >
+                        <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                          <span>
+                            <i className="fas fa-history"></i> Diary
                         </span>
-                      </li>
-                    </NavLink>
+                        </li>
+                      </NavLink>
 
-                    <NavLink
-                      className="nav-link"
-                      to="/about"
-                      activeClassName="current_content"
-                    >
-                      <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                        <span>
-                          <i className="far fa-address-card"></i> About
+                      <NavLink
+                        className="nav-link"
+                        to="/about"
+                        activeClassName="current_content"
+                      >
+                        <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                          <span>
+                            <i className="far fa-address-card"></i> About
                         </span>
-                      </li>
-                    </NavLink>
+                        </li>
+                      </NavLink>
 
-                    <NavLink
-                      className="nav-link"
-                      to="/result"
-                      activeClassName="current_content"
-                    >
-                      <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                        <span>
-                          <i class="fas fa-trophy"></i> Result
+                      <NavLink
+                        className="nav-link"
+                        to="/result"
+                        activeClassName="current_content"
+                      >
+                        <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                          <span>
+                            <i className="fas fa-trophy"></i> Result
                         </span>
-                      </li>
-                    </NavLink>
-                  </ul>
-                </div>
-              </nav>
+                        </li>
+                      </NavLink>
+                      <form>
+                        <input type="search" placeholder="Search" />
+                      </form>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     );
   }
 }
