53941de0f23b94a0e8e09e4a31e66e26518f1ca2
Thanh-Bao <thanh.bao@outlook.com>
2020-06-27 16:14:14 +0700

    fix conflict

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -49,49 +49,6 @@ class Home extends Component {
       </div>
     );
   }
-=======
-    render() {
-
-        return (
-            <div>
-
-                <div className="cont">
-                    <div className="mouse" />
-                    <div className="app">
-                        <div className="app__bgimg">
-                            <div className="app__bgimg-image app__bgimg-image--1">
-                            </div>
-                            <div className="app__bgimg-image app__bgimg-image--2">
-                            </div>
-                        </div>
-                        <div className="app__img">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/537051/whiteTest4.png" alt="" />
-                        </div>
-                        <div className="app__text app__text--1">
-                            <div className="app__text-line app__text-line--4">Welcome </div>
-                            <div className="app__text-line app__text-line--3">HCI_Group_01</div>
-                            <div className="app__text-line app__text-line--2"></div>
-                            <div className="app__text-line app__text-line--1">Năm 2020</div>
-                        </div>
-                        <div className="app__text app__text--2">
-                            <div className="app__text-line app__text-line--4"></div>
-                            <div className="app__text-line app__text-line--3"></div>
-                            <div className="app__text-line app__text-line--2"></div>
-                            <div className="app__text-line app__text-line--1"><img src="" alt="" /></div>
-                        </div>
-                    </div>
-                    <div className="pages">
-                        <ul className="pages__list">
-                            <li data-target={1} className="pages__item pages__item--1 page__item-active" />
-                            <li data-target={2} className="pages__item pages__item--2" />
-                        </ul>
-                    </div>
-                </div>
-
-            </div>
-        );
-    }
->>>>>>> db974efb50072245733e437eb319cb6126e1c772
 }
 
 export default Home;
